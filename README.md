b4acc7b655d5143b720d4202fd22c97552e28ea2
Freewind <nowindlee@gmail.com>
2023-07-18 20:31:49 +0800

    java-maven-avro-demo

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-Java Maven Demo
+Java Maven Avro Demo
 =====================
 
-Use maven for Java.
+Avro: https://avro.apache.org
+
+Avro是一个类似于thrift或者protocol buffers序列化与反序列化框架。
+需要提供一个 `.avsc`文件定义schema，然后用工具生成相应的java/python代码。
+
+它的特点是它已经成为kafka的事实标准，所以如果项目里用到了kafka，一般都会用它。
 
 How to run:
 
-1. Run `Hello.java` in your IDE.
-2. Use maven plugin `exec-maven-plugin`:
-   ```
-   mvn clean compile exec:java
-   ```
-3. Use maven plugin `maven-dependency-plugin` and `maven-jar-plugin`
-   ```
-   mvn clean package; java -jar target/demo.jar
-   ```
+- `maven clean compile` 以根据`.avsc`生成java model代码
+- 在IDE中运行 `Hello.java`
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,11 +1,47 @@
 package demo;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Hello {
 
-  public static void main(String[] args) {
-    System.out.println(StringUtils.trim("Hello World!  "));
-  }
+    public static void main(String[] args) throws IOException {
+        User user1 = new User();
+        user1.setName("Freewind1");
+        user1.setFavoriteNumber(99);
+        user1.setFavoriteColor("black");
+
+        User user2 = User.newBuilder()
+                .setName("Freewind2")
+                .setFavoriteColor("white")
+                .build();
+
+        File dataFile = new File("users.avro");
+        System.out.println("Write to file: " + dataFile);
+
+        try (DataFileWriter<User> dataFileWriter = new DataFileWriter<>(new SpecificDatumWriter<>(User.class))) {
+            dataFileWriter.create(user1.getSchema(), dataFile);
+            // NOTE should append user1 too
+            dataFileWriter.append(user1);
+            dataFileWriter.append(user2);
+        }
+
+        System.out.println("Read from file: " + dataFile);
+        try (DataFileReader<User> dataFileReader = new DataFileReader<>(dataFile, new SpecificDatumReader<>(User.class))) {
+            User user = null;
+            while (dataFileReader.hasNext()) {
+                // Reuse user object by passing it to next(). This saves us from
+                // allocating and garbage collecting many objects for files with
+                // many items.
+                user = dataFileReader.next(user);
+                System.out.println(user);
+            }
+        }
+    }
 
 }
